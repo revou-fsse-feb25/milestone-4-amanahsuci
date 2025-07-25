e4a4b04af9049436fb8d730d03e0a43182a98b12
Amanah Suci <aaamanahsc@gmail.com>
2025-07-25 08:55:49 +0700

    feat: update seed and generate account, transaction, and user resources.

---
+++ prisma/seed.ts
@@ -0,0 +1,92 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const user1 = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'johndoe@revobank.com',
+            password: 'qwerty123',
+    },
+    });
+
+    const user2 = await prisma.user.create({
+        data: {
+            name: 'Alex Doe',
+            email: 'doe.alex@revobank.com',
+            password: 'asdfgh123',
+        },
+    });
+
+    const user3 = await prisma.user.create({
+        data: {
+            name: 'Ella Doe',
+            email: 'elladoe@revobank.com',
+            password: 'zxcvbn1123'
+        },
+    });
+
+    const account1 = await prisma.account.create({
+        data: {
+            userId: user1.id,
+            accountNumber: '000111999222',
+            balance: 1000000000.00,
+        },
+    });
+
+    const account2 = await prisma.account.create({
+        data: {
+            userId: user2.id,
+            accountNumber: '000111999333',
+            balance: 750000000.00,
+        },
+    });
+
+    const account3 = await prisma.account.create({
+        data: {
+            userId: user3.id,
+            accountNumber: '000111999444',
+            balance: 500000000.00,
+        },
+    });
+
+    await prisma.transaction.create({
+        data: {
+            fromAccountId: account1.id,
+            toAccountId: account2.id,
+            amount: 200000.00,
+            type: 'transfer',
+        },
+    });
+
+    await prisma.transaction.create({
+        data: {
+            fromAccountId: account2.id,
+            toAccountId: account3.id,
+            amount: 150000.00,
+            type: 'transfer',
+        },
+    });
+
+    await prisma.transaction.create({
+        data: {
+            fromAccountId: account3.id,
+            toAccountId: account1.id,
+            amount: 100000.00,
+            type: 'transfer',
+        },
+    });
+
+    console.log('✅ Seeding succeded!');
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,    
+    UserModule,
+    AccountModule,
+    TransactionModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
